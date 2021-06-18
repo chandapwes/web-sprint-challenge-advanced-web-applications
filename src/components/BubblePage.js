3da1382940be4044b96307fda6cda7c0f07608d7
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-06-18 17:56:19 -0600

    bubble page worked out

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -5,19 +5,43 @@ import ColorList from "./ColorList";
 
 import { editColorService, deleteColorService } from '../services/colorServices';
 import fetchColorService from '../services/fetchColorService';
+import axiosWithAuth from "../helpers/axiosWithAuth";
+import { setState } from "expect";
+import { STATEMENT_OR_BLOCK_KEYS } from "@babel/types";
 
 const BubblePage = () => {
   const [colors, setColors] = useState([]);
   const [editing, setEditing] = useState(false);
+
+
+  BubblePage.componentDidMount(fetchColorService())
 
   const toggleEdit = (value) => {
     setEditing(value);
   };
 
   const saveEdit = (editColor) => {
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${id}`, editColor)
+    .then(res => {
+      setEditing(editing)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   };
 
   const deleteColor = (colorToDelete) => {
+    axiosWithAuth()
+    .delete(`http://localhost:5000/api/colors/123`)
+    .then(res => {
+      localStorage.removeItem('token', colorToDelete)
+      setColors(colors)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
   };
 
   return (
--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,12 +1,15 @@
-// import axiosWithAuth from '../helpers/axiosWithAuth';
+import axiosWithAuth from '../helpers/axiosWithAuth';
 
 const fetchColorService = (props) => {
-    // axiosWithAuth()
-    // .get(`http://localhost:5000/api/colors`)
-    // .then(res => {
-    //     console.log(res.data)
-    //     props.setState()
-    // })
-}
+    axiosWithAuth()
+    .get(`http://localhost:5000/api/colors`)
+    .then(res => {
+        console.log(res.data)
+        props.setColors([res.data])
+    })
+    .catch(err => {
+        console.log(err)
+    })
+ }
 
 export default fetchColorService;
